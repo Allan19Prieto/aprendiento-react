c0d6a28ef34f1b6ee5ecd65d9ce10875362fac26
Allan Prieto <allanbanilla@gmail.com>
2023-07-17 16:57:58 -0600

    Twitter Card

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+export function App() {
+    return(
+        <article>
+            <header>
+                <img alt="El avatar de un Hombre" src="https://unavatar.io/Lobo" />
+                <div>
+                    <strong>Allan Prieto Badilla</strong>
+                    <span>@allanp</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
